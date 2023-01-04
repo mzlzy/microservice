be0ca9ffaab48354b490ed6c3227c12efddd29c8
mazengli <mazengli@offcn.com>
2023-01-04 14:06:49 +0800

    metadata interceptor

--- grpc_token_auth_test/client/client.go
+++ grpc_token_auth_test/client/client.go
@@ -5,24 +5,45 @@ import (
 	"fmt"
 	"log"
 	"microservice/grpc_test/proto"
-	"time"
 
 	"google.golang.org/grpc/credentials/insecure"
 
 	"google.golang.org/grpc"
 )
 
+type customCredential struct {
+}
+
+func (c *customCredential) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
+	return map[string]string{
+		"appid":  "123456",
+		"appkey": "qwertyuiop",
+	}, nil
+}
+
+func (c *customCredential) RequireTransportSecurity() bool {
+	return false
+}
+
 func main() {
-	interceptor := func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) (err error) {
-		start := time.Now()
-		err = invoker(ctx, method, req, reply, cc, opts...)
-		fmt.Printf("本次请求耗时: %s\n", time.Since(start))
-		return
-	}
+	//interceptor := func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) (err error) {
+	//	start := time.Now()
+	//
+	//	md := metadata.New(map[string]string{
+	//		"appid":  "123456",
+	//		"appkey": "qwertyuiop",
+	//	})
+	//	ctx = metadata.NewOutgoingContext(ctx, md)
+	//
+	//	err = invoker(ctx, method, req, reply, cc, opts...)
+	//	fmt.Printf("本次请求耗时: %s\n", time.Since(start))
+	//	return
+	//}
 
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	opts = append(opts, grpc.WithUnaryInterceptor(interceptor))
+	//opts = append(opts, grpc.WithUnaryInterceptor(interceptor))
+	opts = append(opts, grpc.WithPerRPCCredentials(&customCredential{}))
 	conn, err := grpc.Dial("127.0.0.1:8989", opts...)
 	if err != nil {
 		log.Fatal("grpc dial err: ", err)
--- grpc_token_auth_test/server/server.go
+++ grpc_token_auth_test/server/server.go
@@ -7,6 +7,10 @@ import (
 	"microservice/grpc_test/proto"
 	"net"
 
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+
 	"google.golang.org/grpc"
 )
 
@@ -22,6 +26,23 @@ func (s *Server) SayHello(c context.Context, request *proto.HelloRequest) (*prot
 func main() {
 	interceptor := func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
 		fmt.Println("接收到了请求")
+
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return resp, status.Error(codes.Unauthenticated, "无token认证信息")
+		}
+
+		var appid, appkey string
+		if id, ok := md["appid"]; ok {
+			appid = id[0]
+		}
+		if key, ok := md["appkey"]; ok {
+			appkey = key[0]
+		}
+		if appid != "123456" || appkey != "qwertyuiop" {
+			return resp, status.Error(codes.Unauthenticated, "token认证失败")
+		}
+
 		resp, err = handler(ctx, req)
 		fmt.Println("请求处理完毕")
 		return
